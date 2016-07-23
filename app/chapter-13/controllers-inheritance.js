cdab0c6efcd564c6943badd967e5ed2cc0ad193b
Javier Fraga <javier.s.fraga@gmail.com>
2016-07-22 23:15:53 -0500

    adds final markup

--- app/chapter-13/controllers-inheritance.js
+++ app/chapter-13/controllers-inheritance.js
@@ -7,15 +7,18 @@ app.controller("topLevelCtrl", function ($scope) {
     };
 
     $scope.reverseText = function () {
+		// If an empty string ("") is used as the separator, the string is split between each character
         $scope.data.dataValue = $scope.data.dataValue.split("").reverse().join("");
     };
 
     $scope.changeCase = function () {
         var result = [];
+		// If an empty string ("") is used as the separator, the string is split between each character
         angular.forEach($scope.data.dataValue.split(""), function (char, index) {
             result.push(index % 2 == 1
                 ? char.toString().toUpperCase() : char.toString().toLowerCase());
         });
+		// If an empty string ("") is used as the separator, the string is split between each character
         $scope.data.dataValue = result.join("");
     };
 });
@@ -33,9 +36,11 @@ app.controller("secondChildCtrl", function ($scope) {
 
     $scope.shiftFour = function () {
         var result = [];
+		// If an empty string ("") is used as the separator, the string is split between each character
         angular.forEach($scope.data.dataValue.split(""), function (char, index) {
             result.push(index < 4 ? char.toUpperCase() : char);
         });
+		// If an empty string ("") is used as the separator, the string is split between each character
         $scope.data.dataValue = result.join("");
     }
 });
